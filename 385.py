485aed92f42ddead723655a6e4155735f84bc086
Dofolk <41058334+Dofolk@users.noreply.github.com>
2023-02-13 20:49:26 +0800

    Create 385.py

---
+++ 385.py
@@ -0,0 +1,56 @@
+# 這題是要把進來的巢狀字串變成給定的格式
+# 做法就是先用 eval() 然後再做 NestInteger
+
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def deserialize(self, s: str) -> NestedInteger:
+        def nest(x):
+            if isinstance(x,int):
+                return NestedInteger(x)
+            ls = NestedInteger()
+            for y in x:
+                ls.add(nest(y))
+            return ls
+        return nest(eval(s))
