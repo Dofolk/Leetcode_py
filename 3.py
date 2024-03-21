927c472822603a56ff340830006aedbe08e706c8
Dofolk <41058334+Dofolk@users.noreply.github.com>
2024-03-21 21:08:38 +0800

    Update 3.py
    Revise the explanation.

--- 3.py
+++ 3.py
@@ -1,5 +1,7 @@
 # 這題是要找看看字串中"最長的不重複子字串長度"是多少
-# 作法就是用 two pointer 來做，後面的指標如果找到重複的字的話就更新前面指標的位置
+# 作法就是用 two pointer 來做，p1在前，p2在後
+# 讓p2開始跑並更新最大值，直到p2只到p1~p2區間內重複的字之後就來更新p1的位置，讓字母不重複
+# 一直持續上面步驟直到 p1 p2 都跑到底就知道最長是多少了
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
