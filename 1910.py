f9db5e3473f3940b724ff45d40c51576ff8e936d
Dofolk <41058334+Dofolk@users.noreply.github.com>
2025-02-16 01:04:58 +0800

    Update 1910.py

--- 1910.py
+++ 1910.py
@@ -1,9 +1,9 @@
+# 這題是給一個字串 s 跟要移除的字串 part，如果 part 存在 s 裡面就移除，一直移除到沒有 part 為止，問最後 s 變成甚麼樣子
+# 例子: s = "yababcab", part = "abc"，第一次做就會把 abc拿掉，就得到 s = "yabab"，發現沒有 abc 了就可以回傳 s 了
+# 簡單的做法就是用內建的 str.replace() 來操作，把替換次數設為 1 之後再重複做到不能做就可以了
+
 class Solution:
-      def removeOccurrences(self, s: str, part: str) -> str:
-                string = ""
-                L = len(part)
-                for char in s:
-                              string = ''.join([string, char])
-                              if len(string) >= L and string[-L:] == part:
-                                                string = string[:-L]
-                                        return string
+    def removeOccurrences(self, s: str, part: str) -> str:
+        while part in s:
+            s = s.replace(part, "", 1)
+        return s
